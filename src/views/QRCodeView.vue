44af4430b0bbb6ae829a12ccfa35f9c001fbcff1
PromotezCitizen <gkstkdgus821@gmail.com>
2025-07-27 17:47:11 +0900

    qr코드 컴포넌트 추가

---
+++ src/views/QRCodeView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useQRCode } from '@vueuse/integrations/useQRCode'
+import { shallowRef } from 'vue'
+
+const text = shallowRef('')
+const qrcode = useQRCode(text)
+</script>
+
+<template>
+  <input v-model="text" type="text" />
+  <img :src="qrcode" alt="QR Code" />
+</template>
