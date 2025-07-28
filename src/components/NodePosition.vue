6c99d30702a3c1b877c086ffb2d3cd997d138cc1
PromotezCitizen <gkstkdgus821@gmail.com>
2025-07-28 13:19:57 +0900

    canvas의 모바일 터치 대응 작업
    - 배경 이미지 변경 시 최대 드래그 좌표 계산 로직 수정

--- src/components/NodePosition.vue
+++ src/components/NodePosition.vue
@@ -1,37 +1,33 @@
 <script setup lang="ts">
 import type { SlaveType } from '@/types/slave-type'
 import type Konva from 'konva'
-import { stages } from 'konva/lib/Stage'
 import axiosInstance from '@/utils/axios-instance'
+import { reactive } from 'vue'
 
 const props = defineProps({
-  index: {
-    type: Number,
-    required: true,
-  },
   info: {
     type: Object as () => SlaveType,
-    required: true,
-  },
-  imgSrc: {
-    type: Object as () => HTMLImageElement | null,
     required: true,
   },
   popupPosition: {
     type: Object as () => { x: number; y: number } | null,
     default: null,
   },
+  floorImageSize: {
+    type: Object as () => { width: number; height: number },
+    default: () => ({ width: Number, height: Number }),
+  },
+  selectedMacAddress: {
+    type: String,
+    default: '',
+  },
 })
 
 const emit = defineEmits(['update:nodePosition', 'update:popupPosition', 'update:targetMacAddress'])
 
 const macAddressId = props.info.macAddress.replace(':', '-')
-const rootStage = stages.find((s) => s.id() === 'konva-root-stage')
 const BALL_SIZE = 50
 const macAddress = props.info.macAddress.toLowerCase()
-
-const maxWidth = (rootStage?.width() || 600) - BALL_SIZE
-const maxHeight = (rootStage?.height() || 800) - BALL_SIZE
 
 const defaultVConfig = {
   width: BALL_SIZE,
@@ -44,22 +40,23 @@ const defaultVTextConfig = {
   align: 'center',
 }
 
-const groupConfig = {
+const groupConfig = reactive({
   x: props.info.position.x,
   y: props.info.position.y,
   draggable: true,
-  dragBoundFunc: (pos: Konva.Vector2d) => {
-    return {
-      x: Math.min(maxWidth, Math.max(0, pos.x)),
-      y: Math.min(maxHeight, Math.max(0, pos.y)),
-    }
-  },
+  dragBoundFunc: (pos: Konva.Vector2d) => ({
+    x: Math.min(props.floorImageSize.width - BALL_SIZE, Math.max(0, pos.x)),
+    y: Math.min(props.floorImageSize.height - BALL_SIZE, Math.max(0, pos.y)),
+  }),
   id: `group-${macAddressId}`,
-}
+})
 
 const imgConfig = {
   ...defaultVConfig,
-  image: props.imgSrc,
+  x: BALL_SIZE / 2,
+  y: BALL_SIZE / 2,
+  radius: BALL_SIZE / 2,
+  fill: 'red',
   id: `img-${macAddressId}`,
 }
 
@@ -77,42 +74,55 @@ const commonProps = {
   floor: props.info.position.floor,
 }
 
-const onIconMoveEnd = async (e: Konva.KonvaEventObject<DragEvent>) => {
+const onIconMoveEnd = async (e: Konva.KonvaEventObject<PointerEvent>) => {
   const dom = e.evt
-  const { x, y } = e.target.position()
+  const { x: canvasX, y: canvasY } = e.target.position()
 
   await axiosInstance.patch('/esp32', {
     ...commonProps,
-    positionX: x,
-    positionY: y,
+    positionX: canvasX,
+    positionY: canvasY,
   })
   emit('update:nodePosition', {
     ...commonProps,
-    x,
-    y,
+    x: canvasX,
+    y: canvasY,
   })
-  console.log(props.popupPosition)
-  if (props.popupPosition) updatePopupPosition(dom)
+  if (props.popupPosition && props.info.macAddress === props.selectedMacAddress) {
+    updatePopupPosition(dom)
+  } else {
+    emit('update:popupPosition', null)
+  }
 }
 
-const onClick = (e: Konva.KonvaEventObject<MouseEvent>) => {
+const onClick = (e: Konva.KonvaEventObject<PointerEvent>) => {
   const dom = e.evt
   e.cancelBubble = true
   updatePopupPosition(dom)
   emit('update:targetMacAddress', props.info.macAddress)
 }
 
-const updatePopupPosition = (dom: MouseEvent | DragEvent) => {
+const updatePopupPosition = (dom: PointerEvent) => {
   emit('update:popupPosition', {
-    x: dom.clientX,
-    y: dom.clientY,
+    x:
+      (dom instanceof MouseEvent ? dom.clientX : (dom as TouchEvent).changedTouches[0].clientX) +
+      15,
+    y:
+      (dom instanceof MouseEvent ? dom.clientY : (dom as TouchEvent).changedTouches[0].clientY) +
+      10,
     floor: props.info.position.floor,
   })
 }
 </script>
 <template>
-  <v-group :config="groupConfig" @dragend="onIconMoveEnd" draggable="true" @click="onClick">
-    <v-image :config="imgConfig"></v-image>
+  <v-group
+    :config="groupConfig"
+    @dragend="onIconMoveEnd"
+    draggable="true"
+    @click="onClick"
+    @tap="onClick"
+  >
+    <v-circle :config="imgConfig"></v-circle>
     <v-text :config="macTxtConfig"></v-text>
   </v-group>
 </template>
