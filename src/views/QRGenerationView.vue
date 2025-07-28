bc76c87fd179b3bba40447ad571c69d6791569d2
PromotezCitizen <gkstkdgus821@gmail.com>
2025-07-28 22:47:24 +0900

    결제 QR 생성 페이지 추가
    - 비밀번호 검증을 통해 일반 사용자가 접근하지 못하도록 설정
    - QR 서버 주소 변경 추가

---
+++ src/views/QRGenerationView.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import type { SlaveResponse } from '@/types/slave-type'
+import { axiosInstance } from '@/utils/axios-instance'
+import { useQRCode } from '@vueuse/integrations/useQRCode'
+import { onMounted, ref, watch, type ShallowRef } from 'vue'
+import { useRouter } from 'vue-router'
+
+interface MacType {
+  qr: ShallowRef<string>
+  mac: string
+}
+const router = useRouter()
+
+const slaveMacs = ref<string[]>([])
+const qrs = ref<MacType[]>([])
+
+const baseURL = ref<string>('http://192.168.0.7:8080')
+
+watch([slaveMacs, baseURL], () => {
+  qrs.value = slaveMacs.value.map((mac) => ({
+    qr: useQRCode(`${baseURL.value}/qr-test?mac=${mac}`),
+    mac,
+  }))
+})
+
+onMounted(async () => {
+  const password = prompt('비밀번호를 입력해주세요')
+
+  try {
+    await axiosInstance.post('/admin/login', { password })
+  } catch (e) {
+    console.error(e)
+    router.replace('/')
+    return
+  }
+
+  const { data } = await axiosInstance.get<SlaveResponse>('/esp32/slaves')
+  slaveMacs.value = data.slaves.map((it) => (it.macAddress = it.macAddress.trim().toLowerCase()))
+})
+</script>
+
+<template>
+  <div>
+    <h2>QR 서버 주소 변경</h2>
+    <input type="text" v-model="baseURL" />
+    <div v-for="qr in qrs" :key="qr.mac">
+      <p>{{ qr.mac }}</p>
+      <img :src="qr.qr" />
+    </div>
+  </div>
+</template>
